51acc6afba0bb13919065c4f1906af1d665b3cdc
Sukriti Waani <sukritiwaani2006@gmail.com>
2025-06-19 23:47:53 +0530

    Applied Modules concept

--- 01_Hello_World/hello.js
+++ 01_Hello_World/hello.js
@@ -1,2 +1,10 @@
-console.log("Hey there! I am JS");
+const math = require("./math");
+// const {addFn, subFn} = require("./math");
 
+// console.log("Hey there! I am JS");
+
+// console.log("Addition is: ", math.addFn(2, 4));
+// console.log("Subtraction is: ", math.subFn(6, 4));
+
+console.log("Addition is: ", math.add(2, 4));
+console.log("Subtraction is: ", math.sub(6, 4));
