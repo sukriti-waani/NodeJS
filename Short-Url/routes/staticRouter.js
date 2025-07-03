ea245fd186540f75e4e121315e3d9ebc582733f4
Sukriti Waani <sukritiwaani2006@gmail.com>
2025-07-03 22:20:38 +0530

    Added EJS table to display list of shortened URLs with Serial No, ShortID, Redirect URL, and Clicks

--- Short-Url/routes/staticRouter.js
+++ Short-Url/routes/staticRouter.js
@@ -2,8 +2,11 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  return res.render('home');
-})
+router.get("/", async (req, res) => {
+  const allurls = await URL.find({});
+  return res.render("home", {
+    urls: allurls,
+  });
+});
 
 module.exports = router;
