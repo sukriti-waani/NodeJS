fe54ef3f80c4ecb64031b925d3808d16e6606553
Sukriti Waani <sukritiwaani2006@gmail.com>
2025-07-10 13:09:02 +0530

    Completed

--- Short-Url/routes/staticRouter.js
+++ Short-Url/routes/staticRouter.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const URL = require("../models/url");
 
 const router = express.Router();
 
@@ -6,7 +7,14 @@ router.get("/", async (req, res) => {
   const allurls = await URL.find({});
   return res.render("home", {
     urls: allurls,
+    id: null,
   });
 });
+
+router.get("/delete-all", async (req, res) => {
+  await URL.deleteMany({});
+  res.send("✅ All URL entries have been deleted.");
+});
+
 
 module.exports = router;
