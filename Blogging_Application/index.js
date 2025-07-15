62d4cb838c4f70ac185c14067c225a10f29b909a
Sukriti Waani <sukritiwaani2006@gmail.com>
2025-07-15 19:44:58 +0530

    Navbar Section

--- Blogging_Application/index.js
+++ Blogging_Application/index.js
@@ -7,4 +7,8 @@ const PORT = 8000;
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
-app.listen();
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(PORT, () => console.log(`Server Started at PORT: ${PORT}`));
