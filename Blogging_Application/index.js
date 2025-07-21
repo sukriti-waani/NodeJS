ec49dbf2bf26ae5e2d8e7967a86c99d2c5a9da07
Sukriti Waani <sukritiwaani2006@gmail.com>
2025-07-22 00:39:31 +0530

    Added blogs on homepage

--- Blogging_Application/index.js
+++ Blogging_Application/index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 
+const Blog = require("./models/blog");
+
 const userRoute = require("./routes/user");
 const blogRoute = require("./routes/blog");
 
@@ -26,9 +28,13 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
 
-app.get("/", (req, res) => {
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", async (req, res) => {
+  const allBlogs = await Blog.find({});
   res.render("home", {
     user: req.user,
+    blogs: allBlogs,
   });
 });
 
--- Blogging_Application/routes/blog.js
+++ Blogging_Application/routes/blog.js
@@ -1,10 +1,38 @@
 const { Router } = require("express");
+const multer = require("multer");
+const path = require("path");
+
+const Blog = require("../models/blog");
+
 const router = Router();
 
 router.get("/add-new", (req, res) => {
   return res.render("addBlog", {
-    user: res.user,
+    user: req.user,
   });
+});
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.resolve(`./public/uploads`));
+  },
+  filename: function (req, file, cb) {
+    const filename = `${Date.now()}-${file.originalname}`;
+    cb(null, filename);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+router.post("/", upload.single("coverImage"), async (req, res) => {
+  const { title, body } = req.body;
+  const blog = await Blog.create({
+    title,
+    body,
+    coverImageURL: req.file ? `/uploads/${req.file.filename}` : null,
+    createdBy: req.user._id,
+  });
+  return res.redirect(`/blog/${blog._id}`);
 });
 
 module.exports = router;
